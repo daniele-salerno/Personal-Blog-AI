5a173ad7c5d212ec61bb696047bc8b43a0f5e514
Jotunheim <salerno.daniele92@gmail.com>
2020-11-21 20:37:32 +0100

    config  mongo

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,20 +6,24 @@ from flask_login import LoginManager
 from flask_misaka import Misaka
 
 from config import Config
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+db = MongoEngine()
+db.init_app(app)
+
 login_manager = LoginManager(app)
 Misaka(app)
 
-with app.app_context():
-    if db.engine.url.drivername == 'sqlite':
-        migrate.init_app(app, db, render_as_batch=True)
-    else:
-        migrate.init_app(app, db)
+# with app.app_context():
+#     if db.engine.url.drivername == 'sqlite':
+#         migrate.init_app(app, db, render_as_batch=True)
+#     else:
+#         migrate.init_app(app, db)
     
 
 # da mettere in fondo per evitare gli import circolari
